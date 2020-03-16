68d4461bb821ee71af85d52972052b0d4c7f05b6
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-03-16 14:55:43 +0100

    Récupérer les paramètres du POST

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+const bodyParser = require('body-parser');
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.render('index', { title: "Qui prend quoi ?"});
+});
+
+app.post('/party', function(req, res) {
+    console.log(req.body);
+    res.send("Post OK");
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
