331a7fdd71aa0219ae5d0f4f5a9bc804a7d6d1c1
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-03-17 11:23:40 +0100

    17.c Suppression d'un item

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const axios = require('axios');
 app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Affichage page index
 app.get('/', function (req, res) {
     res.render('index', {title: "Qui prend quoi ?"});
 });
 
+// Création nouvel évènement
 app.post('/party', (req, res) => {
     axios
         .post(`${process.env.API_URL}/party`, req.body)
@@ -18,6 +20,7 @@ app.post('/party', (req, res) => {
         .catch((err) => res.send(err));
 });
 
+// Récupération évènement
 app.get('/party/:id', function (req, res) {
     axios
         .get(`${process.env.API_URL}/party/${req.params.id}`)
@@ -31,10 +34,19 @@ app.get('/party/:id', function (req, res) {
         .catch((err) => console.log(err));
 });
 
+// Création item pour évènement
 app.post('/party/:id/items', (req, res) => {
     axios
         .post(`${process.env.API_URL}/party/${req.params.id}/items`, req.body)
         .then(({data}) => res.redirect(`${process.env.FRONT_URL}:${process.env.PORT}/party/${req.params.id}`))
+        .catch((err) => res.send(err));
+});
+
+// Suppression item pour évènement
+app.post('/party/:party_id/items/:item_id/remove', (req, res) => {
+    axios
+        .delete(`${process.env.API_URL}/party/${req.params.party_id}/items/${req.params.item_id}`, req.body)
+        .then(({data}) => res.redirect(`${process.env.FRONT_URL}:${process.env.PORT}/party/${req.params.party_id}`))
         .catch((err) => res.send(err));
 });
 
