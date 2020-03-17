65adcda7f8dac5bec9c75a21f8f475293a0f515b
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-03-17 09:40:22 +0100

    12 - POST vers l'API et correction

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', function(req, res) {
-    res.render('index', { title: "Qui prend quoi ?"});
+app.get('/', function (req, res) {
+    res.render('index', {title: "Qui prend quoi ?"});
 });
 
-app.post('/party', function(req, res) {
-    console.log(req.body);
-    res.send("Post OK");
+app.post('/party', (req, res) => {
+    axios
+        .post(`${process.env.API_URL}/party`, req.body)
+        .then(({data}) => console.log(data))
+        .catch((err) => console.error(err));
 });
 
-app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
+app.listen(process.env.PORT, () =>
+    console.log(`Front app listening on port ${process.env.PORT}!`)
+);
