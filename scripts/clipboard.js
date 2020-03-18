9674a447c960a5a442112fdc5330ad2072266f37
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-03-18 11:17:07 +0100

    27 - Effectuer la copie

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -4,7 +4,21 @@ if (navigator.clipboard) {
         const $button = document.createElement('button');
         $button.innerHTML = 'Copier';
         $clipboardEl.parentNode.append($button);
+        $button.addEventListener(
+            'click',
+            copyToClipboard.bind(this, $clipboardEl, $button)
+        );
     });
 } else {
     console.warn("Pas de support")
+}
+
+function copyToClipboard($clipboardEl, $button) {
+    navigator.clipboard
+        .writeText($clipboardEl.getAttribute('data-clipboard'))
+        .then(() => {
+            $button.innerHTML = 'Copié !';
+            setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+        })
+        .catch((err) => console.warn(err));
 }
